ec45c4d3c5bbeb68058a154663096d99f07333fb
enzoxdd98 <enzo.xdd@gmail.com>
2024-04-12 01:32:39 +0000

    agregar

---
+++ EvaluacionN1.py
@@ -0,0 +1,11 @@
+nombre = input("Ingrese su nombre: ")
+apellido = input("Ingrese su apellido: ")
+codigo_seccion = input("Ingrese su codigo-seccion: ")
+sede = input("Ingrese su sede: ")
+
+print("\nInformacion Ingresada:")
+print("Nombre:", nombre)
+print("Apellido:", apellido)
+print("Codigo-seccion:", codigo_seccion)
+print("Sede:", sede)
+
